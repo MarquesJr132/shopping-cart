35e5d57bdb0a51d4c4973177a6e3d5761d53ce4c
MarquesJr132 <marquesjr898@gmail.com>
2025-08-13 10:47:26 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/sika-stock-flow-94/' : './',
+  base: mode === 'production' ? '/shopping-cart/' : './',
   server: {
     host: "::",
     port: 8080,
