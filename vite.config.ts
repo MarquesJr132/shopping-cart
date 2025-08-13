db7491122721119575097b553307217d70a00ba5
MarquesJr132 <marquesjr898@gmail.com>
2025-08-13 09:06:06 +0200

    Test 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/sika-stock-flow-94/',
+  base: './',
   server: {
     host: "::",
     port: 8080,
